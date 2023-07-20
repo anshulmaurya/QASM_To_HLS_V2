2ced798a28938d2ee14f79272a251f767aad73de
anshulmaurya <anshulmaurya27@gmail.com>
2023-07-20 18:42:38 -0400

    Final implementation of Rev_1 is tested with Grover's Search and working flawlessly Gate List for Rev_1 : [H ,I, Cnot, not]

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from qiskit import QuantumCircuit
-
+from QASM_Processing import QASMProcessing
+from MatrixGeneration import CircuitListToMatrix
 
 def init(cir, n):
     for p in range(n):
@@ -30,7 +31,6 @@ def oracle(cir, n):
     # cir.barrier([0, 1, 2])
     return cir
 
-
 def diffuser(cir, n):
     for p in range(n):
         cir.h(p)
@@ -47,58 +47,27 @@ def diffuser(cir, n):
         cir.h(p)
     return cir
 
-
-# def simulator(QC, qNum=None, numshots=10000):
-#     if qNum:
-#         QC.measure(qNum, 0)
-#     else:
-#         QC.measure_all()
-#     aer_sim = Aer.get_backend('aer_simulator', device='CPU')
-#     transpiled = transpile(QC, aer_sim)
-#     qobj = assemble(transpiled)
-#     results = aer_sim.run(qobj, shots=numshots).result()
-#     counts = results.get_counts()
-#     return counts
-
-
-gc = QuantumCircuit(4)
-gc.h(0)
-gc.h(1)
-gc.h(2)
-gc.cx([0, 1, 2], 3)
-# gc.ccx(control_qubit1= 0, control_qubit2=1, target_qubit=2)
-# gc.h(1)
-# gc.h(1)
-# gc.x(1)
-
-
-# gc = init(gc, 3)
-# gc = oracle(gc, 3)
-# gc = diffuser(gc, 3)
+gc = QuantumCircuit(3)
+gc = init(gc, 3)
+gc = oracle(gc, 3)
+gc = diffuser(gc, 3)
 print(gc)
-# gc.draw()
-# counts = simulator(gc)
-# plot_histogram(counts, (10, 5))
 
-
-from QASM_Processing import QASMProcessing
-from MatrixGeneration import CircuitListToMatrix
 
 qasm = QASMProcessing(gc)
-# # cirData = qasm.stringProcessing()
-# # cirData = qasm.listProcess
 cirData = qasm.qasmToList()
 print(cirData, len(cirData))
 
 cirMat = CircuitListToMatrix(cirData, qasm.cirQubits)
 matrix = cirMat.genMat
 
-# print(matrix)
 
 
+
+# TESTING WITH KET 0
 import numpy as np
 
-ipVec = np.zeros(2 ** 4)
+ipVec = np.zeros(2 ** 3)
 ipVec[0] = 1
 
 print("\n\nfinal result:\n", np.matmul(ipVec, matrix))
